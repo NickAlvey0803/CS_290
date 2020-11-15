7de00f73949c336b1a650ecaa001905a52de39fb
NickAlvey0803 <59594631+NickAlvey0803@users.noreply.github.com>
2020-11-15 15:55:14 -0800

    Add files via upload

--- alveyn_HW5/helloWorld1.js
+++ alveyn_HW5/helloWorld1.js
@@ -15,6 +15,34 @@ app.get('/other-page',function(req,res){
   res.render('other-page');
 });
 
+app.get('/get-loopback-improved',function(req,res){
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.render('get-loopback-improved', context);
+});
+
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/post-loopback', function(req,res){
+  var qParams = [];
+  for (var p in req.body){
+    qParams.push({'name':p,'value':req.body[p]})
+  }
+  console.log(qParams);
+  console.log(req.body);
+  var context = {};
+  context.dataList = qParams;
+  res.render('post-loopback', context);
+});
+
 app.use(function(req,res){
   res.status(404);
   res.render('404');
