fd0a8eeab62369fdb8cb675f15d983d2d3e0968f
NickAlvey0803 <alveyn@oregonstate.edu>
2020-12-04 18:21:21 -0800

    t

--- HW6/hw6/helloMysql.js
+++ HW6/hw6/helloMysql.js
@@ -23,7 +23,7 @@ app.get('/',function(req,res,next){
       next(err);
       return;
     }
-    // context.results = JSON.stringify(rows);
+    context.results = JSON.stringify(rows);
     res.render('home', context);
   });
 });
